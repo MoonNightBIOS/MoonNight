6c475d6b0849543bf312ff87cca31770a3ba0d40
Mathias Azuri <mathias_azuri@live.com>
2023-11-04 20:24:32 -0300

    Agregando teclas de movimiento 4/11/23

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Variables
+    float movimiento;
+
     void Start()
     {
-        
+        movimiento = 10f;
     }
 
     // Teclas para Movimiento
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3 (movimiento * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(movimiento * Time.deltaTime, 0f, 0f);
+        }
     }
 }
