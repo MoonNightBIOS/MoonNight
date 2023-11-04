bfe7b12e3724795245e79ffad81f24054b6b3233
Mathias Azuri <mathias_azuri@live.com>
2023-11-04 20:12:59 -0300

    Mecanicas de Movimiento

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Teclas para Movimiento
+    void Update()
+    {
+        
+    }
+}
