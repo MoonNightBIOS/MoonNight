2e1182644baa1286e4d932a0c51648c34ac0255d
Mathias Azuri <mathias_azuri@live.com>
2023-11-07 19:03:38 -0300

    Se agrego un Batman para ver animacioens. P = Quieto, C= Caminar, R= Correr

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
+    Animator animaciones;
     // Variables
     float movimiento;
 
     void Start()
-    {
+    { 
+        animaciones = GetComponent<Animator>();
         movimiento = 10f;
     }
 
     // Teclas para Movimiento
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            animaciones.SetInteger("cambioEstado", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animaciones.SetInteger("cambioEstado", 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animaciones.SetInteger("cambioEstado", 2);
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.position -= new Vector3 (movimiento * Time.deltaTime, 0f, 0f);
