7b951c90264cd444f7aed305bfc5d7e08233cd00
RafaelAlberti <rafa.alberti21@gmail.com>
2023-12-20 20:29:02 -0300

    Se agrega brillo

--- Assets/Scripts/menu/menuOpciones.cs
+++ Assets/Scripts/menu/menuOpciones.cs
@@ -10,19 +10,36 @@ public class menuOpciones : MonoBehaviour
     [SerializeField]  float volumenValor;
     [SerializeField]  Image imagenMute;
 
+    [SerializeField] Slider barraBrillo;
+    [SerializeField] float brilloValor;
+    [SerializeField] Image alpha;
+    [SerializeField] Image titulo;
+
+
     void Start()
     {
-        barraVolumen.value = PlayerPrefs.GetFloat("volumenAudio", 1f);
+        barraVolumen.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f);
         AudioListener.volume = barraVolumen.value;
         Mute();
+
+        barraBrillo.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+        alpha.color = new Color(alpha.color.r, alpha.color.g, alpha.color.b, barraBrillo.value);
     }
 
-    public void MoverBarra(float valor)
+    public void MoverBarraVolumen(float valor)
     {
         volumenValor = valor;
         PlayerPrefs.SetFloat("volumenAudio", volumenValor);
         AudioListener.volume = barraVolumen.value;
         Mute();
+    }
+
+    public void MoverBarraBrillo(float valor)
+    {
+        volumenValor = valor;
+        PlayerPrefs.SetFloat("brillo", brilloValor);
+        alpha.color = new Color(alpha.color.r, alpha.color.g, alpha.color.b, barraBrillo.value);
+       
     }
 
     public void Mute()
