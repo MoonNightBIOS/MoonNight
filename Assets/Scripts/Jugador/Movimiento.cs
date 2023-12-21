bd788de9b69382dd60bc06020dd2894a6f7f5ac2
RafaelAlberti <rafa.alberti21@gmail.com>
2023-12-21 19:54:36 -0300

    Fixed bug UI

--- Assets/Scripts/Jugador/Movimiento.cs
+++ Assets/Scripts/Jugador/Movimiento.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movimiento : MonoBehaviour
@@ -9,59 +11,98 @@ public class Movimiento : MonoBehaviour
     Animator animaciones;
     Rigidbody2D rb2D;
     SpriteRenderer personaje;
-    bool giro;
+    BoxCollider2D pies;
+    bool giro = false;
+    bool piso = true;
 
     void Start()
     { 
-        animaciones = GetComponent<Animator>();
-        rb2D = GetComponent<Rigidbody2D>();
-        personaje = GetComponent<SpriteRenderer>();
+        this.animaciones = GetComponent<Animator>();
+        this.rb2D = GetComponent<Rigidbody2D>();
+        this.personaje = GetComponent<SpriteRenderer>();
+        this.pies = GetComponent<BoxCollider2D>();
     }
 
-    // Teclas para Movimiento
+    
     void Update()
     {
+        Moverse();
+        Animaciones();
 
+    }
+
+
+    // MECANICAS
+    public void Moverse()
+    {
         if (Input.GetKey(KeyCode.A)) //MOVIMIENTO A LA IZQUIERDA
         {
             transform.Translate(Vector3.left * velocidad * Time.deltaTime);
-            animaciones.SetInteger("cambioEstado", 3);
             personaje.flipX = true;
             giro = true;
         }
 
-        if (Input.GetKeyUp(KeyCode.A)) // CAMBIO DE ANIMACION A QUIETO
+
+        if (Input.GetKey(KeyCode.D)) // MOVIMIENTO A LA DERECHA
+        {
+            transform.Translate(Vector3.right * velocidad * Time.deltaTime);
+            personaje.flipX = false;
+            giro = false;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space)) // SALTA
+        {
+            if (giro == true)
+            {
+                rb2D.AddForce(new Vector2(-3f, 5f), ForceMode2D.Impulse);
+            }
+            else
+            {
+                rb2D.AddForce(new Vector2(3f, 5f), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Piso")
+        {
+            piso = true;
+        }
+        else
+        {
+            piso = false;
+        }
+    }
+
+    public void Animaciones()
+    {
+        //ANIMACIONES
+
+        if (Input.GetKey(KeyCode.A)) // ANIMACION DE MOVIMIENTO A LA IZQUIERDA
+        {
+            animaciones.SetInteger("cambioEstado", 3);
+        }
+        else if (Input.GetKeyUp(KeyCode.A)) // CAMBIO DE ANIMACION A QUIETO
         {
             animaciones.SetInteger("cambioEstado", 0);
         }
 
         if (Input.GetKey(KeyCode.D)) // MOVIMIENTO A LA DERECHA
         {
-            transform.Translate(Vector3.right * velocidad * Time.deltaTime);
             animaciones.SetInteger("cambioEstado", 3);
-            personaje.flipX = false;
-            giro = false;
         }
-
-        if (Input.GetKeyUp(KeyCode.D)) // CAMBIO DE ANIMACION A QUIETO
+        else if (Input.GetKeyUp(KeyCode.D)) // CAMBIO DE ANIMACION A QUIETO
         {
             animaciones.SetInteger("cambioEstado", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.Space)) // SALTA
         {
-            if (giro == false)
-            {
-                rb2D.AddForce(new Vector2(3f, 5f), ForceMode2D.Impulse);
-                animaciones.SetInteger("cambioEstado", 6);
-            }
-            else
-            {
-                rb2D.AddForce(new Vector2(-3f, 5f), ForceMode2D.Impulse);
-                animaciones.SetInteger("cambioEstado", 6);
-            }
+            animaciones.SetInteger("cambioEstado", 6);
         }
-            
+
         if (Input.GetKey(KeyCode.X)) // ANIMACION DE SE TRANSFORMA
         {
             animaciones.SetInteger("cambioEstado", 1);
@@ -71,7 +112,7 @@ public class Movimiento : MonoBehaviour
         {
             animaciones.SetInteger("cambioEstado", 2);
         }
-        
+
         if (Input.GetKey(KeyCode.Z)) // ANIMACION DE ATAQUE
         {
             animaciones.SetInteger("cambioEstado", 4);
