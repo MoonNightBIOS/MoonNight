2d84a554fd878950ba5210be853fec5b2b37cfda
Mathias Azuri <mathias.azuri91@gmail.com>
2024-02-04 19:44:06 -0300

    Se agrega descripcion de codigos (lobo y humano)

--- Assets/Scripts/Jugador/Movimiento.cs
+++ Assets/Scripts/Jugador/Movimiento.cs
@@ -76,20 +76,20 @@ public class Movimiento : MonoBehaviour
         //ANIMACIONES DEL HOMBRE 
 
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) // ANIMACION DE MOVIMIENTO A LA IZQUIERDA
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) // ANIMACION DE MOVIMIENTO A LA IZQUIERDA (HUMANO)
         {
             animaciones.SetInteger("cambioEstado", 3);
         }
-        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)) // CAMBIO DE ANIMACION A QUIETO
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)) // CAMBIO DE ANIMACION A QUIETO (HUMANO)
         {
             animaciones.SetInteger("cambioEstado", 0);
         }
 
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) // MOVIMIENTO A LA DERECHA
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) // MOVIMIENTO A LA DERECHA (HUMANO)
         {
             animaciones.SetInteger("cambioEstado", 3);
         }
-        else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow)) // CAMBIO DE ANIMACION DE MOVIMIENTO A QUIETO 
+        else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow)) // CAMBIO DE ANIMACION DE MOVIMIENTO A QUIETO (HUMANO)
         {
             animaciones.SetInteger("cambioEstado", 0);
         }
@@ -102,28 +102,28 @@ public class Movimiento : MonoBehaviour
 
         //ANIMACION DEL LOBO
 
-        if (Input.GetKey(KeyCode.X)) // ANIMACION DE SE TRANSFORMA
+        if (Input.GetKey(KeyCode.X)) // ANIMACION PARA LA TRANSFORMACION
         {
             animaciones.SetInteger("cambioEstado", 1);
             animaciones.SetBool("transformacion", true);
         }
 
-        if (animaciones.GetBool("transformacion") == true && Input.GetKey(KeyCode.D)) // ANIMACION DE MOVIMIENTO COMO LOBO
+        if (animaciones.GetBool("transformacion") == true && Input.GetKey(KeyCode.D)) // ANIMACION DE MOVIMIENTO (LOBO)
         {
             animaciones.SetInteger("cambioEstado", 10);
         }
 
-        if (animaciones.GetBool("transformacion") == true && Input.GetKeyUp(KeyCode.D)) // CAMBIO DE ANIMACION DE MOVIMIENTO COMO LOBO COMO IDE LOBO
+        if (animaciones.GetBool("transformacion") == true && Input.GetKeyUp(KeyCode.D)) // CAMBIO DE ANIMACION DE MOVIMIENTO IDLE (LOBO)
         {
             animaciones.SetInteger("cambioEstado", 1);
         }
 
-        if (animaciones.GetBool("transformacion") == true && Input.GetKey(KeyCode.A)) // ANIMACION DE SE TRANSFORMA
+        if (animaciones.GetBool("transformacion") == true && Input.GetKey(KeyCode.A)) // ANIMACION AL MOVERSE (LOBO)
         {
             animaciones.SetInteger("cambioEstado", 10);
         }
 
-        if (animaciones.GetBool("transformacion") == true && Input.GetKeyUp(KeyCode.A)) // ANIMACION DE SE TRANSFORMA
+        if (animaciones.GetBool("transformacion") == true && Input.GetKeyUp(KeyCode.A)) // ANIMACION AL MOVERSE (LOBO)
         {
             animaciones.SetInteger("cambioEstado", 1);
         }
