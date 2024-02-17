ed623a17f13a3c503738b498ddb424c2d20eafca
Mathias Azuri <mathias.azuri91@gmail.com>
2024-02-17 17:59:53 -0300

    Se agrega rayo para detectar al player

--- Assets/Scripts/Enemigos/Enemigos.cs
+++ Assets/Scripts/Enemigos/Enemigos.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemigos : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    RaycastHit2D Obstaculo;
     void Start()
     {
         
@@ -13,6 +15,14 @@ public class Enemigos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+           Obstaculo=Physics2D.Raycast(transform.position, transform.forward, 10f);
+            if (Obstaculo)
+            {
+                Debug.Log("Se detecto Player");
+            }
+        } 
         
     }
 }
